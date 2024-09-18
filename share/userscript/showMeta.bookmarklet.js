e9372a49ed19c322528a71572d5efb7b769b48c2
TakeAsh <take@TakeAsh.net>
2024-09-18 20:54:53 +0900

    add bookmarklet

---
+++ share/userscript/showMeta.bookmarklet.js
@@ -0,0 +1,15 @@
+﻿javascript:
+(async (d) => {
+  const loadScript = (src) => new Promise((resolve, reject) => {
+    const script = d.createElement('script');
+    script.onload = () => { resolve((/([^\/]+)$/.exec(src))[1]); };
+    script.onerror = reject;
+    script.src = src;
+    d.head.appendChild(script);
+  });
+  const results = await Promise.all([
+    'https://www.takeash.net/js/modules/PrepareElement.js',
+    'https://www.takeash.net/GetMeta/userscript/showMeta.user.js',
+  ].map((src) => loadScript(src)));
+  console.log(results);
+})(document);
