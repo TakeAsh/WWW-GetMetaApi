b9115b2c4daa94d463f638a83a8f010f062359c5
TakeAsh <take@TakeAsh.net>
2024-09-20 22:04:12 +0900

    add bookmarklet

--- share/userscript/showMeta.bookmarklet.js
+++ share/userscript/showMeta.bookmarklet.js
@@ -1,5 +1,16 @@
 ﻿javascript:
 (async (d) => {
+  const scripts = [
+    [
+      'https://www.takeash.net/js/modules/Util.js',
+      'https://www.takeash.net/js/modules/PrepareElement.js',
+      'https://www.takeash.net/js/modules/AutoSaveConfig.js',
+      'https://www.takeash.net/js/modules/CyclicEnum.js',
+    ],
+    [
+      'https://www.takeash.net/GetMeta/userscript/showMeta.user.js',
+    ],
+  ];
   const loadScript = (src) => new Promise((resolve, reject) => {
     const script = d.createElement('script');
     script.onload = () => { resolve((/([^\/]+)$/.exec(src))[1]); };
@@ -7,9 +18,8 @@
     script.src = src;
     d.head.appendChild(script);
   });
-  const results = await Promise.all([
-    'https://www.takeash.net/js/modules/PrepareElement.js',
-    'https://www.takeash.net/GetMeta/userscript/showMeta.user.js',
-  ].map((src) => loadScript(src)));
-  console.log(results);
+  for (const group of scripts) {
+    const results = await Promise.all(group.map((src) => loadScript(src)));
+    console.log(results);
+  }
 })(document);
