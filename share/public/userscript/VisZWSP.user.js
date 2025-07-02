37ae245f2c1fe46690661dc270c4417f79bc5771
TakeAsh <take@TakeAsh.net>
2025-07-02 11:23:02 +0900

    add VisZWSP.user.js - ふたったー不具合対策

---
+++ share/public/userscript/VisZWSP.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         VisZWSP
+// @namespace    https://TakeAsh.net/
+// @version      2025-07-01_23:00
+// @description  Visualize Zero Width Space
+// @author       TakeAsh
+// @match        https://*.2chan.net/b/res/*
+// @match        https://tsumanne.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=2chan.net
+// @grant        none
+// ==/UserScript==
+
+((d) => {
+    'use strict';
+    setTimeout(() => {
+        const styleZWSP = d.createElement('style');
+        styleZWSP.textContent = '.ZWSP { background-color: #ff8080; }';
+        d.head.appendChild(styleZWSP);
+        visualize(d);
+        const observer = new MutationObserver(
+            (mutations) => mutations.forEach(
+                (mutation) => visualize(mutation.target)));
+        observer.observe(d, { childList: true, subtree: true, });
+    }, 2000);
+    function visualize(target) {
+        Array.from(target.querySelectorAll('td[class="rtd"], div[data-number]'))
+            .forEach(comment => {
+                comment.innerHTML = comment.innerHTML.replace(
+                    /\u200b|&#8203;|&#x200B;|&ZeroWidthSpace;/giu,
+                    '<span class="ZWSP">ZWSP</span>'
+                );
+            });
+    }
+})(document);
