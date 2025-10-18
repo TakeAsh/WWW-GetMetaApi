950f3232744105da2e9f9918ab2dec800ede14a0
TakeAsh <take@TakeAsh.net>
2025-10-18 19:53:59 +0900

    Mebuki Plus: fix: pin icon over thumbnail

--- share/public/userscript/MebukiPlus.user.js
+++ share/public/userscript/MebukiPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mebuki Plus
 // @namespace    https://TakeAsh.net/
-// @version      2025-10-18_19:30
+// @version      2025-10-18_19:31
 // @description  enhance Mebuki channel
 // @author       TakeAsh
 // @match        https://mebuki.moe/app
@@ -17,7 +17,7 @@
     css.textContent = [
       '.custom-emoji { pointer-events: auto; }',
       '.custom-emoji:hover > .custom-emoji-image { width: initial; height: 6em; position: relative; z-index: 10; }',
-      '.catalog-item:hover { z-index: 5; }',
+      '.catalog-item:hover { z-index: 20; }',
       '.catalog-image { position: relative; }',
       '.catalog-image:hover { width: initial; height: 12em; position: absolute; z-index: 20; }',
     ].join('\n');
