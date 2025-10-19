aecd40fd77ac440b5f5d7cea92a5e5ad77748478
TakeAsh <take@TakeAsh.net>
2025-10-19 16:32:11 +0900

    Mebuki Plus: tune catalog popup position

--- share/public/userscript/MebukiPlus.user.js
+++ share/public/userscript/MebukiPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mebuki Plus
 // @namespace    https://TakeAsh.net/
-// @version      2025-10-19_03:00
+// @version      2025-10-19_16:00
 // @description  enhance Mebuki channel
 // @author       TakeAsh
 // @match        https://mebuki.moe/app
@@ -20,7 +20,7 @@
   css.textContent = [
     '.custom-emoji { pointer-events: auto; }',
     '.custom-emoji:hover > .custom-emoji-image { width: initial; height: 6em; position: relative; z-index: 10; }',
-    '.catalog-item:hover { z-index: 20; }',
+    '.catalog-item:hover { transform: translate(50%,50%) translate(-6em,-6em); z-index: 20; }',
     '.catalog-image { position: relative; }',
     '.catalog-image:hover { width: initial; height: 12em; position: absolute; z-index: 20; }',
   ].join('\n');
