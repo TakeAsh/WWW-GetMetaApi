fa71294de2e9ca4d4959ce5c34837b2fc8da71ec
TakeAsh <take@TakeAsh.net>
2025-10-23 20:20:58 +0900

    Mebuki Plus: Zorome picker - integrate watchCatalog() and watchThread()

--- share/public/userscript/MebukiPlus.user.js
+++ share/public/userscript/MebukiPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mebuki Plus
 // @namespace    https://TakeAsh.net/
-// @version      2025-10-19_19:00
+// @version      2025-10-23_20:00
 // @description  enhance Mebuki channel
 // @author       TakeAsh
 // @match        https://mebuki.moe/app
@@ -23,10 +23,10 @@
     '.catalog-item:hover { transform: translate(50%,50%) translate(-6em,-6em); z-index: 20; }',
     '.catalog-image { position: relative; }',
     '.catalog-image:hover { width: initial; height: 12em; position: absolute; z-index: 20; }',
+    '.zorome { color: #ff0000; font-size: 125%; }',
   ].join('\n');
   d.head.appendChild(css);
-  watchCatalog();
-  watchThread();
+  watch();
 
   function sleep(ms, resolve) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -53,45 +53,44 @@
     );
     return emojis;
   }
-  function watchCatalog() {
-    const target = d.body; // d.querySelector('.catalog-auto-wrapper, ul[class*="grid-cols-"]');
-    if (!target) {
-      console.log('No Catalog');
-      return;
-    }
+  function watch() {
     const modify = (target) => {
+      // Catalog
+      // Thread thumbnail popup
       Array.from(target.querySelectorAll('.catalog-item'))
-        .filter(elm => !elm.dataset.mod)
+        .filter(elm => !elm.dataset.checkThreadThumbnail)
         .forEach(elm => {
-          elm.dataset.mod = 1;
+          elm.dataset.checkThreadThumbnail = 1;
           elm.title = elm.querySelector('.text-sm').textContent;
         });
-    };
-    modify(target);
-    const observer = new MutationObserver(
-      (mutations) => mutations.forEach(
-        (mutation) => modify(mutation.target)));
-    observer.observe(target, { childList: true, subtree: true, });
-  }
-  function watchThread() {
-    const target = d.body; // d.querySelector('.thread-messages');
-    if (!target) {
-      console.log('No Thread');
-      return;
-    }
-    const modify = (target) => {
+
+      // Thread
+      // Emoji popup
       Array.from(target.querySelectorAll('.custom-emoji-image'))
-        .filter(elm => !elm.dataset.mod)
+        .filter(elm => !elm.dataset.checkEmoji)
         .forEach(elm => {
-          elm.dataset.mod = 1;
+          elm.dataset.checkEmoji = 1;
           const key = elm.src.replace(/^[\s\S]+\/([^\/\.]+)\.\w+$/, '$1');
           elm.title = emojis[key] || key;
         });
+      // Zorome picker
+      Array.from(target.querySelectorAll('.text-sm > .text-xs'))
+        .filter(elm => !elm.dataset.checkZorome)
+        .forEach(elm => {
+          elm.dataset.checkZorome = 1;
+          const after = elm.innerHTML.replace(
+            /(\.\d*?)((\d)\3+)$/,
+            '$1<span class="zorome">$2</span>'
+          );
+          if (elm.innerHTML != after) {
+            elm.innerHTML = after;
+          }
+        });
     };
-    modify(target);
+    modify(d.body);
     const observer = new MutationObserver(
       (mutations) => mutations.forEach(
         (mutation) => modify(mutation.target)));
-    observer.observe(target, { childList: true, subtree: true, });
+    observer.observe(d.body, { childList: true, subtree: true, });
   }
 })(window, document);
