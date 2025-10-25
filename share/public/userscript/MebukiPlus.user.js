238567d3b4202032bd800d784c071d28a14fa382
TakeAsh <take@TakeAsh.net>
2025-10-25 23:56:13 +0900

    Mebuki Plus: add setting panel, Dice(RGB)

--- share/public/userscript/MebukiPlus.user.js
+++ share/public/userscript/MebukiPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mebuki Plus
 // @namespace    https://TakeAsh.net/
-// @version      2025-10-25_18:30
+// @version      2025-10-26_00:00
 // @description  enhance Mebuki channel
 // @author       TakeAsh
 // @match        https://mebuki.moe/app
@@ -9,7 +9,6 @@
 // @require      https://raw.githubusercontent.com/TakeAsh/js-Modules/main/modules/Util.js
 // @require      https://raw.githubusercontent.com/TakeAsh/js-Modules/main/modules/PrepareElement.js
 // @require      https://raw.githubusercontent.com/TakeAsh/js-Modules/main/modules/AutoSaveConfig.js
-// @require      https://raw.githubusercontent.com/TakeAsh/js-Modules/main/modules/CyclicEnum.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mebuki.moe
 // @grant        none
 // ==/UserScript==
@@ -18,6 +17,21 @@
   'use strict';
   const urlCustomEmoji = 'https://mebuki.moe/api/custom-emoji';
   const urlEmoji = 'https://mebuki.moe/assets/emoji-data-CJuCqmpZ.js';
+  const settings = new AutoSaveConfig({
+    ZoromePicker: true,
+    Dice: {
+      RGB: true,
+    },
+  }, 'MebukiPlusSettings');
+  const Dice = {
+    RGB: {
+      Reg: /(?<dice>RGB値?[\s\S]+?dice3d255=[\s\S]*?>(?<r>\d+)\s(?<g>\d+)\s(?<b>\d+)\s\(\d+\)<[^>]+>)/giu,
+      Replace: (m, html) => html.replace(
+        m.groups['dice'],
+        `${m.groups['dice']} <span style="background-color: rgb(${m.groups['r']},${m.groups['g']},${m.groups['b']});">　　　</span>`
+      ),
+    },
+  };
   const emojis = await getEmojis();
   await sleep(2000);
   addStyle({
@@ -38,6 +52,23 @@
     },
     '.zorome': {
       color: '#ff0000', fontSize: '125%',
+    },
+    '#MebukiPlus_Main': {
+      marginBottom: 'auto',
+    },
+    '#MebukiPlus_Summary': {
+      textAlign: 'right',
+    },
+    '#MebukiPlus_Body': {
+      background: 'var(--card)',
+    },
+    '#MebukiPlus_Body > fieldset': {
+      border: 'solid 0.15em',
+      margin: '0.5em',
+      padding: '0em 0.5em',
+    },
+    '#MebukiPlus_Body > fieldset > div': {
+      display: 'grid',
     },
   });
   watch();
@@ -66,6 +97,96 @@
   }
   function watch() {
     const modify = (target) => {
+      // Panel
+      const header = d.body.querySelector('main > header > div');
+      if (!header.querySelector('#MebukiPlus_Main')) {
+        header.appendChild(prepareElement({
+          tag: 'div',
+          id: 'MebukiPlus_Main',
+          children: [{
+            tag: 'details',
+            children: [
+              {
+                tag: 'summary',
+                id: 'MebukiPlus_Summary',
+                innerHTML: '&#x1f331;+',
+                title: 'Mebuki Plus',
+              },
+              {
+                tag: 'div',
+                id: 'MebukiPlus_Body',
+                children: [
+                  {
+                    tag: 'fieldset',
+                    children: [
+                      {
+                        tag: 'legend',
+                        textContent: 'ゾロ目',
+                      },
+                      {
+                        tag: 'div',
+                        children: [
+                          {
+                            tag: 'label',
+                            children: [
+                              {
+                                tag: 'input',
+                                type: 'checkbox',
+                                name: 'ZoromePicker',
+                                checked: settings.ZoromePicker,
+                                events: {
+                                  change: (ev) => { settings.ZoromePicker = ev.currentTarget.checked; },
+                                },
+                              },
+                              {
+                                tag: 'span',
+                                textContent: 'ピックアップ',
+                              },
+                            ],
+                          },
+                        ],
+                      }
+                    ],
+                  },
+                  {
+                    tag: 'fieldset',
+                    children: [
+                      {
+                        tag: 'legend',
+                        textContent: 'ダイス',
+                      },
+                      {
+                        tag: 'div',
+                        children: [
+                          {
+                            tag: 'label',
+                            children: [
+                              {
+                                tag: 'input',
+                                type: 'checkbox',
+                                name: 'DiceRGB',
+                                checked: settings.Dice.RGB,
+                                events: {
+                                  change: (ev) => { settings.Dice.RGB = ev.currentTarget.checked; },
+                                },
+                              },
+                              {
+                                tag: 'span',
+                                textContent: 'RGB',
+                              },
+                            ],
+                          },
+
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          }],
+        }));
+      }
       // Catalog
       // Thread thumbnail popup
       Array.from(target.querySelectorAll('.catalog-item'))
@@ -85,18 +206,38 @@
           elm.title = emojis[key] || key;
         });
       // Zorome picker
-      Array.from(target.querySelectorAll('.text-sm > .text-xs'))
-        .filter(elm => !elm.dataset.checkZorome)
-        .forEach(elm => {
-          elm.dataset.checkZorome = 1;
-          const after = elm.innerHTML.replace(
-            /(\.\d*?)((\d)\3+)$/,
-            '$1<span class="zorome">$2</span>'
-          );
-          if (elm.innerHTML != after) {
-            elm.innerHTML = after;
-          }
-        });
+      if (settings.ZoromePicker) {
+        Array.from(target.querySelectorAll('.text-sm > .text-xs'))
+          .filter(elm => !elm.dataset.checkZorome)
+          .forEach(elm => {
+            elm.dataset.checkZorome = 1;
+            const after = elm.innerHTML.replace(
+              /(\.\d*?)((\d)\3+)$/,
+              '$1<span class="zorome">$2</span>'
+            );
+            if (elm.innerHTML != after) {
+              elm.innerHTML = after;
+            }
+          });
+      }
+      // Dice
+      // RGB
+      if (Object.keys(settings.Dice).some(key => settings.Dice[key])) {
+        Array.from(target.querySelectorAll('.message-content'))
+          .filter(elm => !elm.dataset.checkDice)
+          .forEach(elm => {
+            elm.dataset.checkDice = 1;
+            let m;
+            Object.keys(settings.Dice)
+              .filter(key => settings.Dice[key])
+              .forEach(key => {
+                const dice = Dice[key];
+                while (m = dice.Reg.exec(elm.innerHTML)) {
+                  elm.innerHTML = dice.Replace(m, elm.innerHTML);
+                }
+              });
+          });
+      }
     };
     watchTarget(modify, d.body);
   }
