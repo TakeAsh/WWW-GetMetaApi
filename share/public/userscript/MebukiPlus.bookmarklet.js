2d6a2d6c0f0537aa6be9307b4be4e9e3d5b96388
TakeAsh <take@TakeAsh.net>
2025-11-01 20:51:04 +0900

    Mebuki Plus: add bookmarklet

---
+++ share/public/userscript/MebukiPlus.bookmarklet.js
@@ -0,0 +1,27 @@
+﻿javascript:
+(async (d) => {
+  const scripts = [
+    [
+      'https://www.takeash.net/js/modules/Util.js',
+      'https://www.takeash.net/js/modules/PrepareElement.js',
+      'https://www.takeash.net/js/modules/AutoSaveConfig.js',
+    ],
+    [
+      'https://www.takeash.net/GetMeta/userscript/MebukiPlus.user.js',
+    ],
+  ];
+  const loadScript = (src) => new Promise((resolve, reject) => {
+    const name = (/([^\/]+)$/.exec(src))[1];
+    const script = d.createElement('script');
+    script.onload = () => { resolve(name); };
+    script.onerror = reject;
+    if (name.endsWith('.mjs')) {
+      script.type = 'module';
+    }
+    script.src = src;
+    d.head.appendChild(script);
+  });
+  for (const group of scripts) {
+    console.log(await Promise.all(group.map((src) => loadScript(src))));
+  }
+})(document);
